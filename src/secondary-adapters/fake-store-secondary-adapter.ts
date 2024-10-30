bac21e57a6914e5aa3f5be49783e492f92fe873b
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-10-30 19:50:54 +0100

    implements FakeStoreSecondaryAdapter

--- src/secondary-adapters/fake-store-secondary-adapter.ts
+++ src/secondary-adapters/fake-store-secondary-adapter.ts
@@ -1,3 +1,8 @@
-export class FakeStoreSecondaryAdapter {
-  
+import { StoreGreetingSecondaryPort } from "../business/greeting-service";
+
+export class FakeStoreSecondaryAdapter implements StoreGreetingSecondaryPort {
+    
+    save(name: string){
+        console.log(`Saved: ${name}`);
+    }
 }
