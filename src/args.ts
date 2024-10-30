664e1eb01c87c01199c74b8d56d460dd6810b07e
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-10-30 19:58:07 +0100

    Glues the FakeStoreSecondaryAdapter and GreetingService together
    npm run ts -- src/args.ts Bobbbbbb

--- src/args.ts
+++ src/args.ts
@@ -1,3 +1,8 @@
 import { runArgsApp } from "./primary-adapters/args-input-primary-adapter";
 import { FakeStoreSecondaryAdapter } from "./secondary-adapters/fake-store-secondary-adapter";
 import { GreetingService } from './business/greeting-service';
+
+const storeAdapter = new FakeStoreSecondaryAdapter();
+const greetingService = new GreetingService(storeAdapter)
+runArgsApp(greetingService);
+
