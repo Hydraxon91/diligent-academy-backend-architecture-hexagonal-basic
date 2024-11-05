8242dedabce41c2cbe7107c7eb2f31bf6efffacf
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-11-05 16:10:01 +0100

    Adds and implements ProvideNamePromptPrimaryPort interface

--- src/business/greeting-service.ts
+++ src/business/greeting-service.ts
@@ -7,9 +7,30 @@ export interface StoreGreetingSecondaryPort {
   save: (greeting: string) => void;
 }
 
+export interface ProvideNamePromptPrimaryPort {
+  greet: (name: string) => string;
+}
+
+export interface StoreGreetingInTxtSecondaryPort {
+  save: (greeting: string) => void;
+}
+
 export class GreetingService implements ProvideNamePrimaryPort{
 
   constructor(private readonly store: StoreGreetingSecondaryPort){}
+
+  greet(name: string) {
+    if (!name) {
+      throw new Error('Please provide a name')
+    }
+    const greeting = `Hello, ${name}`;
+    this.store.save(greeting);
+    return greeting;
+  }
+}
+
+export class PromptService implements ProvideNamePromptPrimaryPort{
+  constructor(private readonly store: StoreGreetingInTxtSecondaryPort){}
 
   greet(name: string) {
     if (!name) {
