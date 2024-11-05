79c2218b33ae301b3564b0164498d4c27a393a64
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-11-05 16:46:39 +0100

    Adds app.post('greet') to fastify server

--- src/primary-adapters/fastify-api-primary-adapter.ts
+++ src/primary-adapters/fastify-api-primary-adapter.ts
@@ -1,11 +1,29 @@
 import fastify from 'fastify';
+import { JsonStoreSecondaryAdapter } from '../secondary-adapters/json-store-secondary-adapter';
+import { FastifyService } from '../business/greeting-service';
 
 export default function createApp(options = {}) {
   const app = fastify(options)
+  const jsonStore = new JsonStoreSecondaryAdapter();
+  const fastifyService = new FastifyService(jsonStore);
 
   app.get('/hello', async () => {
     return { hello: 'World!' }
   })
+
+  app.post('/greet', async (request, reply) => {
+    const { name } = request.body as { name: string };
+    try {
+      const response = fastifyService.greet(name);
+      return reply.send(response);
+    } catch (error) {
+      if (error instanceof Error) {
+        reply.status(400).send({ error: error.message });
+      } else {
+        reply.status(400).send({ error: 'An unexpected error occurred' });
+      }
+    }
+  });
 
   return app;
 }
