e83750ab9104b33edd9ede53f41fd4d73f3567da
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-11-05 16:10:46 +0100

    Implements TxtStoreSecondaryAdapter interface

--- src/secondary-adapters/txt-store-secondary-adapter.ts
+++ src/secondary-adapters/txt-store-secondary-adapter.ts
@@ -1,1 +1,24 @@
-export class TxtStoreSecondaryAdapter {}
+import { existsSync, writeFileSync, appendFileSync } from "fs";
+
+export class TxtStoreSecondaryAdapter implements TxtStoreSecondaryAdapter{
+
+    save(message: string){
+        if (!message) {
+            throw new Error('Please provide a name')
+        }
+
+        const filePath = './greetings.txt';
+
+        try {
+            if(!existsSync(filePath)){                
+                writeFileSync(filePath, `${message}\n`, 'utf-8')
+            }
+            else{
+                appendFileSync(filePath, `${message}\n`, 'utf-8');
+            }
+            console.log(`Saved: ${message}`); 
+        } catch (error) {
+            throw new Error('something went wrong saving the file')
+        }
+    }
+}
