96ce802301b32073bb656a4af726f0bbd8e52bf6
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-11-05 16:46:22 +0100

    Implements FastifyService

--- src/business/greeting-service.ts
+++ src/business/greeting-service.ts
@@ -11,6 +11,11 @@ export interface ProvideNamePromptPrimaryPort {
   greet: (name: string) => string;
 }
 
+export interface HttpGreetingPrimaryPort {
+  greet: (name: string) => { greeting: string };
+}
+
+
 export class GreetingService implements ProvideNamePrimaryPort{
 
   constructor(private readonly store: StoreGreetingSecondaryPort){}
@@ -35,5 +40,17 @@ export class PromptService implements ProvideNamePromptPrimaryPort{
     const greeting = `Hello, ${name}`;
     this.store.save(greeting);
     return greeting;
+  }
+}
+
+export class FastifyService implements HttpGreetingPrimaryPort {
+  constructor(private readonly store: StoreGreetingSecondaryPort){}
+  greet(name: string) {
+    if (!name) {
+      throw new Error('Please provide a name')
+    }
+    const greeting = `Hello, ${name}`;
+    this.store.save(greeting);
+    return {greeting};
   }
 }
